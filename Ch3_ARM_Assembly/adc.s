93c8fb7f3dbc73ad280d9f3b2318fb3844f975f9
sangyun01 <worldtop0612@gmail.com>
2025-04-09 18:05:56 +0900

    ads.s comment done

---
+++ Ch3_ARM_Assembly/adc.s
@@ -0,0 +1,41 @@
+    .data
+string: .asciz "The result is %X%X\n"
+@ %X => 16진수 대문자 출력 
+@ 255 / %x -> ff / %X -> FF
+
+    .text
+    .global main
+    .extern printf
+
+main:
+    push {lr}
+
+    @ 강의듣고 틀린 부분 있으면 고치기
+    mov r5, #0xFFFFFFFFFF @ overflow 발생 => 0xFFFFFFFF 로 32bit로 제한
+    mov r6, #0xFFFFFFFFFF @ overflow 발생 => 0xFFFFFFFF 로 32bit로 제한
+
+    adds r7, r5, r6 @ r7 = r5 + r6을 하고 캐리를 저장한다.
+    @ r5 = 1111 / 1111 / 1111 / 1111 / 1111 / 1111 / 1111 / 1111 
+    @ r6 = 1111 / 1111 / 1111 / 1111 / 1111 / 1111 / 1111 / 1111
+
+    @ r7 = 1(carry로 빠짐) || 1111(F) / 1111(F) / 1111(F) / 1111(F) / 1111(F) / 1111(F) / 1111(F) / 1110(E)
+    @ r7 => 0xFFFFFFFE
+
+    mov r8, #0xFF
+    mov r9, #0xFF
+    adc r10, r8, r9
+    @ r8 = 1111 / 1111
+    @ r9 = 1111 / 1111
+    @ r10 = r8 + r9 + 1
+    @ r8 + r9
+    @ => 1 / 1111 / 1110
+    @ r8 + r9 + 1
+    @ => 1 / 1111(F) / 1111(F)
+    @ r10 = 1FF
+
+    ldr r0, =string
+    mov r1, r10 @1FF
+    mov r2, r7 @FFFFFFFE
+    
+    bl printf
+    pop {pc}
