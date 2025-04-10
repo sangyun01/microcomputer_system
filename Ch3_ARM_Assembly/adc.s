72b460e1db18e417f57384f2b3280262bea07681
sangyun01 <worldtop0612@gmail.com>
2025-04-10 22:55:34 +0900

    add concept

--- Ch3_ARM_Assembly/adc.s
+++ Ch3_ARM_Assembly/adc.s
@@ -10,9 +10,9 @@ string: .asciz "The result is %X%X\n"
 main:
     push {lr}
 
-    @ 강의듣고 틀린 부분 있으면 고치기
-    mov r5, #0xFFFFFFFFFF @ overflow 발생 => 0xFFFFFFFF 로 32bit로 제한
-    mov r6, #0xFFFFFFFFFF @ overflow 발생 => 0xFFFFFFFF 로 32bit로 제한
+    @ 32bit 2개를 이용하여 64bit 계산 과정
+    mov r5, #0xFFFFFFFF @ overflow 발생 => 0xFFFFFFFF 로 32bit로 제한
+    mov r6, #0xFFFFFFFF @ overflow 발생 => 0xFFFFFFFF 로 32bit로 제한
 
     adds r7, r5, r6 @ r7 = r5 + r6을 하고 캐리를 저장한다.
     @ r5 = 1111 / 1111 / 1111 / 1111 / 1111 / 1111 / 1111 / 1111 
@@ -21,8 +21,8 @@ main:
     @ r7 = 1(carry로 빠짐) || 1111(F) / 1111(F) / 1111(F) / 1111(F) / 1111(F) / 1111(F) / 1111(F) / 1110(E)
     @ r7 => 0xFFFFFFFE
 
-    mov r8, #0xFF
-    mov r9, #0xFF
+    mov r8, #0xFF   @ r5에서 남은 0xFF
+    mov r9, #0xFF   @ r6에서 남은 0xFF
     adc r10, r8, r9
     @ r8 = 1111 / 1111
     @ r9 = 1111 / 1111
