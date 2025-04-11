5913886912157c4c6070079a6c30da8d68fcf577
sangyun01 <worldtop0612@gmail.com>
2025-04-11 21:28:22 +0900

    modify comment pointer.s

--- Ch3_ARM_Assembly/pointer.s
+++ Ch3_ARM_Assembly/pointer.s
@@ -1,6 +1,8 @@
     .data
 string: .asciz "The result is %p (TABLE1) and %p (TABLE2)\n"
-@ %p -> 주소값 출력력
+@ %p -> 주소값 출력
+@ %0x와 동일한 형태로 출력된다 - 둘다 16진수이므로
+
     .align                      @ 데이터 정렬
 TABLE1: .word 10, 20, 30        @ TABLE1의 배열 [10, 20, 30]
 TABLE2: .word 0, 0, 0           @ TABLE2의 배열 [0, 0, 0]
@@ -20,5 +22,9 @@ main:
     mov r1, r5      @ r1에 r5값 저장            
     mov r2, r6      @ r2에 r6값 저장
 
-    bl printf
+    bl printf       @ r1과 r2의 주소가 출력된다.
+                    @ if r1의 주소가 0x21054라고 하자
+                    @ word는 3개를 받았다. -> 4byte * 3 = 12byte
+                    @ 0x21054 + 12 => 0x21060이 된다.
+                    @ 따라서 r2의 시작주소는 0x21060이다.
     pop {pc}
