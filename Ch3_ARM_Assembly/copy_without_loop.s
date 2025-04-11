f3c3f8281764c94364ed50af45ce21680370860b
sangyun01 <worldtop0612@gmail.com>
2025-04-11 21:38:30 +0900

    done week 6

--- Ch3_ARM_Assembly/copy_without_loop.s
+++ Ch3_ARM_Assembly/copy_without_loop.s
@@ -1,7 +1,8 @@
     .data
 string: .asciz "The result is %d, %d and %d\n"
+        @ TABLE2의 값이 순차적으로 출력될 것이다.
 
-    .align
+    .align                  @ 정렬
 TABLE1: .word 10, 20, 30
 TABLE2: .word 0, 0, 0
 
@@ -12,22 +13,23 @@ TABLE2: .word 0, 0, 0
 main:
     push {lr}
 
-    ldr r5, =TABLE1
-    ldr r6, =TABLE2
+    ldr r5, =TABLE1     @ r5에 TABLE1 주소 저장
+    ldr r6, =TABLE2     @ r6에 TABLE2 주소 저장
 
-    ldr r7, [r5]
-    str r7, [r6]
+    ldr r7, [r5]        @ r7에 주소 r5에 있는 값을 저장한다.
+    str r7, [r6]        @ r7의 값을 주소 r6의 위치에 저장한다.
 
-    ldr r7, [r5, #4]
-    str r7, [r6, #4]
+    ldr r7, [r5, #4]    @ r7에 주소 r5 + 4의 위치에 있는 값을 저장한다.
+    str r7, [r6, #4]    @ r7의 값을 주소 r6 + 4의 위치에 저장한다.
 
-    ldr r7, [r5, #8]
-    str r7, [r6, #8]
+    ldr r7, [r5, #8]    @ r7에 주소 r5 + 8의 위치에 있는 값을 저장한다.
+    str r7, [r6, #8]    @ r7의 값을 주소 r6 + 8 위치에 저장한다.
 
-    ldr r0, =string
-    ldr r1, [r6]
-    ldr r2, [r6, #4]
-    ldr r3, [r6, #8]
+    ldr r0, =string     
+                        @ r6은 TABLE2의 주소이다.
+    ldr r1, [r6]        @ r6의 주소에 있는 값을 호출한다.
+    ldr r2, [r6, #4]    @ r6 + 4의 주소에 있는 값을 호출한다.
+    ldr r3, [r6, #8]    @ r6 + 8의 주소에 있는 값을 호출한다.
 
     bl printf
     
