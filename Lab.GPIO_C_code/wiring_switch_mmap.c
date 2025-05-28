f1ecece0935cf500c4f9bfc1d56d22a39dbd89bc
sangyun01 <worldtop0612@gmail.com>
2025-05-28 10:23:36 +0900

    done mmap_code.c

---
+++ Lab.GPIO_C_code/wiring_switch_mmap.c
@@ -0,0 +1,43 @@
+// write the terminal the code
+// $gcc wiring_switch_mmap.c -o wiring_switch_mmap
+// sudo ./wiring_switch_mmap
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include "gpio.h"
+
+#define LED 18  // LED is light element -> write
+#define SW 24   // Switch is user input element -> read
+
+int switchControl(void);
+
+int main(void) {
+    wiringPiSetupGpio();
+    switchControl();
+
+    return 0;
+}
+
+int switchControl(void) {
+    time_t start, current;
+    int cnt = 0;
+    start = time(NULL);
+
+    pinMode(LED, OUTPUT);
+    pinMode(SW, INPUT);
+
+    while (difftime(time(NULL), start) < 30) {
+        if(digitalRead(SW) == LOW) {
+            digitalWrite(LED, HIGH);     // LED ON, HIGH = 1
+            printf("Switch is pressed (count : %d)\r\n", ++cnt);
+        }
+        else {
+            digitalWrite(LED, LOW);     // LED OFF, LOW = 0
+        }
+        delay(1000);
+    }
+}
